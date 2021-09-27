ad29db58d7223a91caaf8ae2079f9532eeee87cf
psadalja <parthadalja29@gmail.com>
2021-09-27 23:35:58 +0530

    user input

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -4,13 +4,16 @@ const AddUser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
-  <form onSubmit={addUserHandler}>
+  return(
+
+      <form onSubmit={addUserHandler}>
     <label htmlFor="username">User Name</label>
     <input id="username" type="text" />
     <label htmlFor="age">Age (Years)</label>
     <input id="age" type="number" />
     <button type="submit">Add User</button>
   </form>;
+  )
 };
 
 export default AddUser;
