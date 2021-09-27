5a6f6f89646ad7310a897a41e90d07e529696d8f
psadalja <parthadalja29@gmail.com>
2021-09-27 23:34:35 +0530

    user input box

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const AddUser = (props) => {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+  <form onSubmit={addUserHandler}>
+    <label htmlFor="username">User Name</label>
+    <input id="username" type="text" />
+    <label htmlFor="age">Age (Years)</label>
+    <input id="age" type="number" />
+    <button type="submit">Add User</button>
+  </form>;
+};
+
+export default AddUser;
